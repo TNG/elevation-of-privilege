e3c2296dfba395cb3ce54244d2a368935ed965f7
Christoph Niehoff <christoph.niehoff@tngtech.com>
2024-06-28 09:55:07 +0200

    Fix prettier warnings
    Signed-off-by: Christoph Niehoff <christoph.niehoff@tngtech.com>

--- src/client/pages/create.tsx
+++ src/client/pages/create.tsx
@@ -452,9 +452,7 @@ class Create extends React.Component<CreateProps, CreateState> {
                     onChange={this.updateImage}
                     checked={this.state.modelType === ModelType.IMAGE}
                   />
-                  <FormText color="muted">
-                    (Max. 5mb)
-                  </FormText>
+                  <FormText color="muted">(Max. 5mb)</FormText>
                 </FormGroup>
                 <FormGroup>
                   <Label check>
