694d9d7c8762c9cec7ce6f1034c14266c75536e5
Vishal Gupta <vishalguptahmh@gmail.com>
2024-08-07 03:11:03 +0530

    working with Threat dragon 2
    Signed-off-by: Vishal Gupta <vishalguptahmh@gmail.com>

--- packages/shared/src/utils/constants.ts
+++ packages/shared/src/utils/constants.ts
@@ -1,6 +1,8 @@
 import type { ThreatDragonModel } from '../game/ThreatDragonModel';
+import type { ThreatDragonModel2 } from '../game/ThreatDragonModel2';
 
 export enum ModelType {
+  THREAT_DRAGON_V2 = 'Threat DragonV2',
   THREAT_DRAGON = 'Threat Dragon',
   PRIVACY_ENHANCED = 'Default',
   IMAGE = 'Image',
@@ -12,6 +14,50 @@ export const CARD_LIMIT = 26;
 export const MIN_NUMBER_PLAYERS = 2;
 export const MAX_NUMBER_PLAYERS = 9;
 export const DEFAULT_TURN_DURATION = 300;
+
+export const TM2 : ThreatDragonModel2 = {
+  summary: {
+    title: 'Threat Modelling',
+    owner: 'Owner',
+    description: 'Description',
+    id: 0,
+  },
+  detail: {
+    contributors: [],
+    diagrams: [
+      {
+        title: 'Threat Modelling',
+        thumbnail: '',
+      
+        id: 0,
+        cells: [
+          {
+            shape: 'Actor',
+            zIndex: 1,
+            id: '90cdcc2d-21ab-443d-ae95-f97a798429e7',
+            data: {
+              name: 'Application',
+              description: '',
+              type: 'tm.Actor',
+              isTrustBoundary: false,
+              hasOpenThreats: false,
+            },
+            position: {
+              x: 50,
+              y: 50,
+            },
+          },
+        ],
+        version: '1',
+      },
+    ],
+    reviewer: 'Reviewer',
+    diagramTop: 0,
+    threatTop: 0,
+  },
+  version: '1',
+};   
+
 
 export const DEFAULT_MODEL: ThreatDragonModel = {
   summary: {
