c77ff993ab8ad6c179fec1c8655685349f58a206
Christoph Niehoff <christoph.niehoff@tngtech.com>
2024-11-19 09:43:16 +0100

    make logo clickable
    Signed-off-by: Christoph Niehoff <christoph.niehoff@tngtech.com>

--- apps/client/src/components/logo/logo.tsx
+++ apps/client/src/components/logo/logo.tsx
@@ -1,11 +1,13 @@
 import type React from 'react';
 import { useNavigate } from 'react-router-dom';
+import classes from './logo.module.css';
 
 const Logo: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <img
+      className={classes['logo']}
       src="logo.png"
       alt="logo"
       height="120px"
