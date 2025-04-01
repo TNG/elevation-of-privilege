99ed5a23ce0d8ccca631ef43436b385ce9198b40
Gareth <37451439+ledge39@users.noreply.github.com>
2025-04-01 16:23:15 +0100

    Update cardDefinitions.ts

--- packages/shared/src/utils/cardDefinitions.ts
+++ packages/shared/src/utils/cardDefinitions.ts
@@ -179,7 +179,7 @@ export const CARD_DECKS: CardDeckDefinitions = {
     C: { name: 'Repudiation', abbreviation: 'R', cards: ['C2','C3','C4','C5','C6','C7','C8','C9','C10','CJ','CQ','CK','CA','CE','CF','CG','CH'], isTrump: false, isDefault: false },
     D: { name: 'Information Disclosure', abbreviation: 'I', cards: ['D2','D3','D4','D5','D6','D7','D8','D9','D10','DJ','DQ','DK','DA','DE','DF'], isTrump: false, isDefault: false },
     E: { name: 'Denial of Service', abbreviation: 'DoS', cards: ['E2','E3','E4','E5','E6','E7','E8','E9','E10','EJ','EQ','EK','EA','EE'], isTrump: false, isDefault: false },
-    T: { name: 'Elevation of Privilege', abbreviation: 'EoP', cards: ['T2','T3','T4','T5','T6','T7','T8','T9','T10','TJ','TQ','TK','TA'], isTrump: truer, isDefault: false },
+    T: { name: 'Elevation of Privilege', abbreviation: 'EoP', cards: ['T2','T3','T4','T5','T6','T7','T8','T9','T10','TJ','TQ','TK','TA'], isTrump: true, isDefault: false },
     F: { name: 'Transfer', abbreviation: 'TR', cards: ['F2','F3','F4','F5','F6','F7','F8','F9','FA'], isTrump: false, isDefault: false },
     G: { name: 'Retention', abbreviation: 'RE', cards: ['G2','G3','G4','G5','G6','G7','G8','G9','G10','GJ','GA'], isTrump: false, isDefault: false },
     H: { name: 'Inference', abbreviation: 'INF', cards: ['H2','H3','H4','H5','H6','H7','H8','H9','H10','HA'], isTrump: false, isDefault: false },
